431ed5dce7fccac526b5f957b82f81c545273ed8
Temurbek <temurbeknuriddinov9@gmail.com>
2022-11-09 09:14:11 +0500

    Funksiyalardagi codlarni keraksiz qismi olib tashlandi

--- Funksiya/index.php
+++ Funksiya/index.php
@@ -3,8 +3,7 @@
 $n = 6;
     function son($a){
     
-        // if($a <= $k)
-        // {
+        
             if($a==0){
                 return 0;
             }
@@ -18,12 +17,6 @@ $n = 6;
                 return son($a-1)+son($a-2) ;
             }
       
-        // $a3 = $a1 + $a2 ;
-        // $a2 = $a1 ;
-        // $a1 = $a3 ;
-        // echo $a3 ." " ;
-
-    //}
 }
         
     for($i = 0 ; $i <= $n ; $i++)   
